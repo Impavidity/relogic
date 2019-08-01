994f25efca4471fad83797848375ad09e1e4c409
Impavidity <spone.2014@gmail.com>
2019-08-01 10:14:45 -0400

    sync structures

--- relogic/structures/span.py
+++ relogic/structures/span.py
@@ -8,7 +8,7 @@ from relogic.structures.linkage_candidate import LinkageCandidate
 @dataclass
 class Span(Structure):
   text: str = None
-  _text: field(init=False, repr=False) = None
+  _text: str = field(init=False, repr=False)
   tokens: List[Token] = field(default_factory=list)
   start_index: int = None
   end_index: int = None
@@ -35,6 +35,10 @@ class Span(Structure):
       self.tokens.append(Token(text=token))
     else:
       self.tokens.append(token)
+    if self._text is not None:
+      # Because the sentence has been mutated, the text string
+      # needs to be regenerated.
+      self._text = None
 
   def set_start_index(self, start_index: int):
     self.start_index = start_index
@@ -46,12 +50,17 @@ class Span(Structure):
     self.wikipedia_uri = uri
 
   @property
-  def text(self) -> str:
+  def text(self):
     if self._text is None:
       self._text = " ".join([token.text for token in self.tokens])
     return self._text
   
   @text.setter
-  def text(self, text: str):
-    self._text = text
-  
+  def text(self, text):
+    if isinstance(text, property):
+      # This is a ugly fix for non-arguments initialization
+      # s = Span()
+      # TODO: find better solution for this implementation
+      self._text = None
+    else:
+      self._text = text
