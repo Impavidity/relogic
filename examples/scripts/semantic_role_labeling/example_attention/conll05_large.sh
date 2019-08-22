bcb2d0bbf067bccfc81f26980ea7dc8b17c8bfd8
Impavidity <spone.2014@gmail.com>
2019-08-22 11:01:17 -0400

    bash example for joint srl

--- examples/scripts/semantic_role_labeling/example_attention/conll05_large.sh
+++ examples/scripts/semantic_role_labeling/example_attention/conll05_large.sh
@@ -1,7 +1,7 @@
 python -u -m relogic.main  \
 --task_name joint_srl \
 --mode train \
---output_dir saves/semantic_role_labeling/conll05_large_joint_1 \
+--output_dir saves/semantic_role_labeling/conll05_large_joint_fixed_two_stage_optim_3000_warmup_fix_eval \
 --bert_model bert-large-cased \
 --raw_data_path data/raw_data/srl/json/conll05/tuple_label \
 --train_file train.json \
@@ -10,8 +10,8 @@ python -u -m relogic.main  \
 --label_mapping_path data/preprocessed_data/srl_conll05_label_mapping.json \
 --model_name default \
 --local_rank $1 \
---train_batch_size 16 \
---test_batch_size 16 \
+--train_batch_size 12 \
+--test_batch_size 12 \
 --learning_rate 3e-5 \
 --epoch_number 3 \
 --eval_dev_every 1000 \
@@ -20,7 +20,7 @@ python -u -m relogic.main  \
 --srl_module_type span_gcn \
 --span_inference \
 --gradient_accumulation_steps 2 \
+--two_stage_optim \
+--adam_learning_rate 0.005 \
 # --external_embeddings data/embeddings/wiki-news-300d-1M.npy \
 # --use_span_candidates \
-# --two_stage_optim \
-# --adam_learning_rate 0.005 \
