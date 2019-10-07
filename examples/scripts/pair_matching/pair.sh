9e1ad01d324e6c7edd891e015e94451568415197
Impavidity <spone.2014@gmail.com>
2019-10-07 17:50:39 -0400

    pair matching scripts

---
+++ examples/scripts/pair_matching/pair.sh
@@ -0,0 +1,16 @@
+python -u -m relogic.main \
+--task_name pairwise \
+--mode train \
+--output_dir saves/pair_matching/1 \
+--bert_model bert-base-multilingual-cased \
+--raw_data_path data/raw_data/entity_alignment \
+--label_mapping_path none \
+--model_name default \
+--local_rank $1 \
+--train_batch_size 4 \
+--test_batch_size 4 \
+--learning_rate 3e-5 \
+--epoch_number 2 \
+--lang zh \
+--eval_dev_every 3000 \
+--max_seq_length 250 \
