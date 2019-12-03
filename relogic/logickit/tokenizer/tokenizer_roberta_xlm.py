78f40a2ed944299723eef38fbf29fd8491607959
Impavidity <spone.2014@gmail.com>
2019-12-03 17:34:00 -0500

    fix roberta xlm tokenization issue

--- relogic/logickit/tokenizer/tokenizer_roberta_xlm.py
+++ relogic/logickit/tokenizer/tokenizer_roberta_xlm.py
@@ -12,12 +12,19 @@ class RobertaXLMTokenizer:
   def from_pretrained(cls, pretrained_model_name_or_path):
     return cls(model=pretrained_model_name_or_path)
 
+  # def special_match(self, other_tok, bpe_tok):
+  #   if other_tok == '΄' and bpe_tok == "́":
+  #     return True
+  #   return False
+
   def alignment(self, bpe_tokens, expected_tokenization):
     bpe_tokens = [clean(text) for text in bpe_tokens]
     alignment = []
+    bpe_toks_list = list(filter(lambda item: item[1] != '', enumerate(bpe_tokens, start=1)))
     bpe_toks = filter(lambda item: item[1] != '', enumerate(bpe_tokens, start=1))
     j, bpe_tok = next(bpe_toks)
     for other_tok in expected_tokenization:
+      other_tok = other_tok.replace('΄', "́")
       bpe_indices = []
       while True:
         if other_tok.startswith(bpe_tok):
@@ -34,7 +41,7 @@ class RobertaXLMTokenizer:
           other_tok = ''
         else:
           print(bpe_tokens)
-          print(bpe_toks)
+          print(bpe_toks_list)
           print(expected_tokenization)
           raise Exception('Cannot align "{}" and "{}"'.format(other_tok, bpe_tok))
         if other_tok == '':
@@ -75,6 +82,9 @@ class RobertaXLMTokenizer:
     return fixed_bpe_tokens, is_head, fixed_roberta_ids
 
 if __name__ == "__main__":
-  sent = "286 Hidemichi Tanaka ( Japan ) 66 75 75 70 , Steve Jones 70 69"
+  sent = " ".join(['Μεγάλης', 'κλίμακας', 'αναστηλωτικές', 'εργασίες', 'έγιναν', 'από', 'τη', 'Γαλλική',
+                   'Αρχαιολογική', 'Σχολή', ',', 'κυρίως', 'στις', 'συνοικίες', 'ιδιωτικών', 'κατοικιών',
+                   ',', 'ενώ', 'μικρότερης', 'κλίμακας', 'πραγματοποιούνται', 'τα', 'τελευταία', 'χρόνια',
+                   'από', 'την', 'ΚΑ΄', 'Εφορεία', 'Προϊστορικών', 'και', 'Κλασικών', 'Αρχαιοτήτων', '.'])
   tokenizer = RobertaXLMTokenizer("xlmr.large.v0")
   raise NotImplementedError("Test")
