e94fd5db843548206b146e205055a460964d7df7
Impavidity <spone.2014@gmail.com>
2020-01-21 15:48:29 -0500

    follow sentence embedding paper

--- relogic/logickit/modules/matching_module.py
+++ relogic/logickit/modules/matching_module.py
@@ -11,7 +11,7 @@ class MatchingModule(nn.Module):
     else:
       self.n_classes = n_classes
     if self.config.ir_siamese:
-      hidden_size = config.hidden_size * 2
+      hidden_size = config.hidden_size * 3
     else:
       hidden_size = config.hidden_size
     self.to_logits = nn.Linear(hidden_size, self.n_classes)
@@ -21,7 +21,7 @@ class MatchingModule(nn.Module):
     if self.config.ir_siamese:
       a_features = kwargs.pop("a_features")
       b_features = kwargs.pop("b_features")
-      logits = self.to_logits(torch.cat([a_features[:, 0], b_features[:, 0]], dim=-1))
+      logits = self.to_logits(torch.cat([a_features[:, 0], b_features[:, 0], torch.abs(a_features[:, 0]-b_features[:, 0])], dim=-1))
     else:
       features = kwargs.pop("features")
       logits = self.to_logits(features[:,0])
